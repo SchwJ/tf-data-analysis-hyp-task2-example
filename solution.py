03b7a0702eecdc97df00e3ea02e04ccb27f54687
SchwJ <67754964+SchwJ@users.noreply.github.com>
2023-04-14 01:55:07 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,17 @@
 import pandas as pd
-import numpy as np
-from scipy.stats import cramervonmises_2samp, anderson_ksamp
 
+import numpy as np
+
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 749357762 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
+
     # Измените код этой функции
+
     # Это будет вашим решением
+
     # Не меняйте название функции и её аргументы
-    return anderson_ksamp([x, y]).significance_level < 0.09 # Ваш ответ, True или False
+
+    return cramervonmises_2samp(x, y).pvalue < 0.09 # Ваш ответ, True или False
